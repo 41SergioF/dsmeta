1014dd1a600b293764d18a61d4738df89b0af643
41SergioF <carlossergio2401@outook.com>
2022-09-07 16:56:08 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
-import NotificationButton from "./components/NotificationButton "
+import Header from "./components/Header";
+import NotificationButton from "./components/NotificationButton ";
 
 function App() {
   return (
-   <>
-    <h1>Olá mundo</h1>
-    <NotificationButton/>
-   </>
-  )
+    <>
+      <Header />
+      <NotificationButton />
+      <p>sa</p>
+    </>
+  );
 }
 
-export default App
+export default App;
