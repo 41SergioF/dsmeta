7907b3fecb45a7a85a16cacf11052a5a2ebe126f
41SergioF <carlossergio2401@outook.com>
2022-09-09 19:33:07 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,12 +1,24 @@
-import icon from '../../assets/img/notification-icon.svg'
-import './style.css'
+import axios from "axios";
+import icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
+import "./style.css";
 
-function NotificationButton() {
-    return (
-        <div className="dsmeta-red-btn">
-            <img src={icon} alt="Notificar"/>
-        </div>
-    )
+type Props = {
+  saleId: number;
+};
+
+function handleClick(id: number) {
+  axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
+    console.log("Deu certo");
+  });
+}
+
+function NotificationButton({ saleId }: Props) {
+  return (
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+      <img src={icon} alt="Notificar" />
+    </div>
+  );
 }
 
 export default NotificationButton;
--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -21,8 +21,8 @@ function SalesCard() {
     const dmin = minDate.toISOString().slice(0, 10);
     const dmax = maxDate.toISOString().slice(0, 10);
 
-    axios
-      .get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
+    axios(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
+    //get()`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
       .then((response) => {
         setSales(response.data.content);
         console.log("return server: ",response.data.content);
@@ -79,7 +79,7 @@ function SalesCard() {
                   <td>R$ {sale.amount.toFixed(2)}</td>
                   <td>
                     <div className="dsmeta-red-btn-container">
-                      <NotificationButton />
+                      <NotificationButton saleId={sale.id}/>
                     </div>
                   </td>
                 </tr>
